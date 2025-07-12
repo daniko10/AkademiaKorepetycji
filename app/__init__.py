fe5d5a6c33d55c9d627b322ca7728708036fd004
Daniel <dczapla79@gmail.com>
2025-07-13 01:51:21 +0200

    delete unused import

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
-from flask_socketio import SocketIO
 import os
 
 app = Flask(__name__, template_folder='../templates')
