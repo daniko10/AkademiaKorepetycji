67ea0c8e1b879b35e55aa7c0b6bf3c2b25d30784
Daniel <dczapla79@gmail.com>
2025-07-13 17:13:05 +0200

    Disable PDF compression

--- app/utils.py
+++ app/utils.py
@@ -13,12 +13,3 @@ def compress_file(filepath):
                      quality=70)
         except Exception as e:
             print("Image compress error:", e)
-
-    else:
-        gz_path = filepath + '.gz'
-        try:
-            with open(filepath, 'rb') as f_in, gzip.open(gz_path, 'wb') as f_out:
-                f_out.writelines(f_in)
-            os.replace(gz_path, filepath)
-        except Exception as e:
-            print("GZIP compress error:", e)
