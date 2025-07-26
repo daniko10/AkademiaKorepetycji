49484303beddb24659822f14baf7955cd907c411
Daniel <dczapla79@gmail.com>
2025-07-26 13:13:59 +0200

    chat | teacher dashboard

--- app/models.py
+++ app/models.py
@@ -56,3 +56,14 @@ class Task(db.Model):
 
 class Administrator(UserBase, UserMixin):
     __tablename__ = 'administrators'
+
+class Message(db.Model):
+    __tablename__ = 'messages'
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, nullable=False)
+    receiver_id = db.Column(db.Integer, nullable=False)
+    sender_role = db.Column(db.String(20), nullable=False)
+    receiver_role = db.Column(db.String(20), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    
